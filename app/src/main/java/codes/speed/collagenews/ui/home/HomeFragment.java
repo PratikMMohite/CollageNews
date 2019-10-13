372015347a977cbd5ad1c31b07d1bdbb8838ff0a
Pratik Mohite <32018912+mohitepm1998@users.noreply.github.com>
2019-10-13 16:26:37 +0530

    sign up added

--- app/src/main/java/codes/speed/collagenews/ui/home/HomeFragment.java
+++ app/src/main/java/codes/speed/collagenews/ui/home/HomeFragment.java
@@ -7,30 +7,25 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import codes.speed.collagenews.R;
 
 public class HomeFragment extends Fragment {
 
-
-//    private HomeViewModel homeViewModel;
+    RecyclerView recyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        //  homeViewModel =ViewModelProviders.of(this).get(HomeViewModel.class)
-//        final TextView textView = root.findViewById(R.id.text_home);
-
-//        homeViewModel.getText().observe(this, new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
-
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+        recyclerView = root.findViewById(R.id.list);
+        HomeViewModel adapter = new HomeViewModel();
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
 
-
+        recyclerView.setAdapter(adapter);
         return root;
     }
 }
--- app/src/main/java/codes/speed/collagenews/ui/home/HomeViewModel.java
+++ app/src/main/java/codes/speed/collagenews/ui/home/HomeViewModel.java
@@ -1,20 +1,50 @@
 package codes.speed.collagenews.ui.home;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class HomeViewModel extends ViewModel {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
-    private MutableLiveData<String> mText;
+import codes.speed.collagenews.R;
 
-    public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("HOME PAGE");
+public class HomeViewModel extends RecyclerView.Adapter<HomeViewModel.Viewholder> {
+    private String[] jack = {"jakc", "asd", "adkj", "daksdj"};
+
+    @NonNull
+    @Override
+    public Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem = layoutInflater.inflate(R.layout.listitem, parent, false);
+        Viewholder vv = new Viewholder(listItem);
+
+        return vv;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Viewholder holder, int position) {
+
+        holder.text.setText(String.valueOf(position));
+
 
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    @Override
+    public int getItemCount() {
+
+        return 100;
+    }
+
+    public static class Viewholder extends RecyclerView.ViewHolder {
+        public TextView text;
+
+        public Viewholder(@NonNull View itemView) {
+            super(itemView);
+            this.text = itemView.findViewById(R.id.textView8);
+
+
+        }
     }
 }
